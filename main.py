bf6d5d212bf67ebd6fd518f8731f2cc0d8d82939
eric.gao <eric.gao@rock-chips.com>
2016-09-23 19:42:34 +0800

    Add file list send function and send command ,local command function

--- main.py
+++ main.py
@@ -10,12 +10,20 @@ Best regard for you.
 @author: Eric.gao
 @time  : 2016/9/22
 
-'''  
-import pexpect  
+''' 
 
-def send_file(ip,user,mypassword):
+# Import everything we need 
+import pexpect 
+
+# User login information 
+user = 'root'  
+ip = '192.168.1.104'  
+mypassword = 'test0000'
+
+# Function send_file: send one file every time 
+def send_file(ip,user,mypassword,filename,dest):
     
-    child = pexpect.spawn('scp %s %s@%s:/' % ( './test.txt',user,ip ))  
+    child = pexpect.spawn('scp %s %s@%s:%s' % ( filename,user,ip,dest ))  
     try: 
         child.expect ('Password:')  
         child.sendline (mypassword)
@@ -25,11 +33,64 @@ def send_file(ip,user,mypassword):
    
     child.expect(pexpect.EOF)
     print child.before
-
-if __name__ == '__main__':  
-    user = 'root'  
-    ip = '192.168.1.104'  
-    mypassword = 'test0000'  
     
-    send_file(ip,user,mypassword)  
-   
+
+# Function send_cmd: send one 
+def send_cmd(user,ip,cmd,mypassword): 
+
+    child = pexpect.spawn('ssh %s@%s %s' % (user,ip,cmd))
+    try:
+        i = child.expect(['Password:','yes','shit'])
+        if i == 0:
+            child.sendline (mypassword)
+        elif i == 1:
+            child.sendline ('yes')
+            child.expect ('Password:')
+            child.sendline (mypassword)
+        elif i == 2:
+            print 'Time Out Error\r\n'
+    except pexpect.EOF:
+        child.close()
+
+    #child.expect(pexpect.EOF)
+    print "\nsend cmd '"+cmd+"'"
+
+# Function local_cmd: run a local cmd 
+def local_cmd(cmd,mypassword): 
+
+    child = pexpect.spawn('%s' % (cmd))
+    try:
+        i = child.expect(['Password:','yes','shit'])
+        if i == 0:
+            child.sendline (mypassword)
+        elif i == 1:
+            child.sendline ('yes')
+            child.expect ('Password:')
+            child.sendline (mypassword)
+        elif i == 2:
+            print 'Time Out Error\r\n'
+    except pexpect.EOF:
+        child.close()
+
+    #child.expect(pexpect.EOF)
+    print "\nlocal cmd '"+cmd+"'"
+
+# The main function
+if __name__ == '__main__':  
+ 
+    # file list that will send to dest machine
+    filelist = [
+	['test.sh','/usr/bin/'],
+	['test.mkv','/var/log/']
+    ]
+
+    local_cmd('rm /home/cros/.ssh/known_hosts','cros')
+    
+    send_cmd(user,ip,'mount -o remount,rw /',mypassword)
+    
+    for one_file in filelist:
+        send_file(ip,user,mypassword,one_file[0],one_file[1])  
+    
+    send_cmd(user,ip,'test.sh',mypassword)
+
+ 
